6023d8b0003f6341f42e1ea65d5edfde76ae2745
DannySvoboda <155640918+DannySvoboda@users.noreply.github.com>
2024-05-23 13:05:49 +0000

    Cyber_News_Fix1

--- script.js
+++ script.js
@@ -1,12 +1,9 @@
 function openNav() {
   document.getElementById("mySidenav").style.width = "40%";
   document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
-  document.getElementById("Devtag").style.position = "fixed";
 }
 
 function closeNav() {
   document.getElementById("mySidenav").style.width = "0";
   document.body.style.backgroundColor = "mediumpurple";
-  document.getElementById("mySidenav.a.Devtag").style.position = "relative"
-
 }
