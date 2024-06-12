abc4a1b6aef5993ce6f8055534cfe2040ba7afb7
DannySvoboda <155640918+DannySvoboda@users.noreply.github.com>
2024-06-12 13:07:40 +0000

    Update 6/12/24

--- script.js
+++ script.js
@@ -1,9 +1,24 @@
 function openNav() {
-  document.getElementById("mySidenav").style.width = "40%";
   document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+  document.getElementById("mySidenav").style.width = "40%";
 }
 
 function closeNav() {
-  document.getElementById("mySidenav").style.width = "0";
   document.body.style.backgroundColor = "mediumpurple";
+  document.getElementById("mySidenav").style.width = "0";
+}
+
+function openNavNoCChange() {
+  document.getElementById("mySidenav").style.width = "40%";
+}
+
+function closeNavNoCChange() {
+  document.getElementById("mySidenav").style.width = "0";
+}
+
+function checkCode() {
+  const key = "124a"
+  if (key == document.getElementById("Code").value.toLowerCase()){
+    document.getElementById("gate").style.display = "block";
+  }
 }
