175296e6643317ca71e59586e5d3a0955a8ecf2c
DannySvoboda <155640918+DannySvoboda@users.noreply.github.com>
2024-06-13 11:47:15 +0000

    Removed console statements from js game code

--- script.js
+++ script.js
@@ -33,7 +33,6 @@ var current_word = ""
 
 function select_word(){
   var select_word = word_array[Math.floor(Math.random() * word_array.length)]
-  console.log(select_word)
   scramble(select_word)
   current_word = select_word
 }
@@ -42,8 +41,7 @@ function scramble(base_word){
   var letter_list = []
   for (let i = 0; i < base_word.length; i++)
     letter_list.push(base_word.charAt(i))
-  console.log("Original: " + letter_list)
-
+  
   for (let i = 0; i <= 4; i++){
     randint = Math.floor(Math.random()*4)
     letter_list.push(letter_list[randint])
@@ -56,16 +54,13 @@ function reassemble(l_list){
   var scrambled_word = ""
   scrambled_word = l_list.join()
   scrambled_word = scrambled_word.replace(/,/g, "");
-  console.log("New: " + scrambled_word)
   document.getElementById("S_word").textContent = scrambled_word
 }
 
 function check_answer(scrambled_word){
   if (document.getElementById("Answer").value.toLowerCase() == current_word){
-    console.log("You Passed!")
     document.getElementById("passfail").textContent = "You Passed!"
   }else{
-    console.log("Try Again!")
     document.getElementById("passfail").textContent = "Try Again!"
   }
 }
